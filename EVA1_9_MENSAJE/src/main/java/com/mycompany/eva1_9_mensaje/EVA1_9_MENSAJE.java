e60b676efd491dd7eddb0c339cbfc20b74bd2abf
Saulpinon01 <124391888+Saulpinon01@users.noreply.github.com>
2023-02-21 19:26:17 -0600

    Add files via upload

---
+++ EVA1_9_MENSAJE/src/main/java/com/mycompany/eva1_9_mensaje/EVA1_9_MENSAJE.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_9_mensaje;
+
+/**
+ *
+ * @author saulp
+ */
+public class EVA1_9_MENSAJE {
+
+    public static void main(String[] args) {
+       nPrintLn("HOLA",3);
+    }
+    public static void nPrintLn(String message,int num){
+        for (int i = 0; i < num; i++) {
+            System.out.println(message);
+            
+        }
+    }
+}
